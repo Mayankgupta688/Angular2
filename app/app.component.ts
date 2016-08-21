600c64edb316716051f9e188587e808a6d700f3a
MayankGupta <MayankGupta688@Gmail.com>
2016-08-21 14:16:08 +0530

    çhanges'

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,22 @@
 import { Component } from '@angular/core';
+
+export class Employee {
+  name: string;
+  designation: string;
+}
+
 @Component({
-  selector: 'my-app',
-  template: '<h1>My First Angular 2 App</h1>'
+  selector: 'show-details',
+  template: `<h1>{{title}}</h1>
+  	<h2><label>Employee Name: </label>{{employee.name}}</h2>
+	<h2><label>Employee Designation: </label>{{employee.designation}}</h2>
+	`
 })
-export class AppComponent { }
+
+export class AppComponent {
+	title = 'Employee Details';
+	employee: Employee = {
+		name: 'Mayank Gupta',
+		designation: 'Senior Software Developer'
+	}
+}
